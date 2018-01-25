fbb5076c3c9df9b0a1ce7d71665c57e4b092e324
3ndG4me <kmiller@gustr.com>
2018-01-24 20:28:13 -0500

    PropAcc enabled message in Hello World default plugin
    Resolves issue #14

---
+++ Propane/PropAcc/hello_world/__init__.py
@@ -0,0 +1,6 @@
+def start():
+    # print("Hello World!")
+    print("It looks like you have enabled PropAccs!")
+    print("If you did not mean to do this, you can disable them in the propane_config.ini file.")
+    print("Otherwise, if you don't wish to see this message again, please delete the default 'hello_world' plugin from the PropAcc folder.")
+    print("We hope you enjoy your PropAccs!")
